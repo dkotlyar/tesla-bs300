2a36df85495fe2c6e4dc194b872a8dcec821c33d
Dmitry Kotlyar <33775104+dkotlyar@users.noreply.github.com>
2019-01-22 17:12:20 +0300

    ????????? ???? ? ??????????? ? ?????????? ????????. ????????? ????????????? ????????? ???? ?? ?????????????? ??????

--- TESLA-BS300/TESLA-BS300/Form1.Designer.cs
+++ TESLA-BS300/TESLA-BS300/Form1.Designer.cs
@@ -45,6 +45,9 @@
             this.distanceMeasurementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.areaMeasurementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clearMeasurementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.signalCharacterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.signalCharacterResetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
@@ -62,6 +65,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.verticalRulerPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.horizontalRulerPictureBox)).BeginInit();
@@ -69,6 +75,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.teslaStatePictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.horizontalLine)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.verticalLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // e2010BackgroundWorker
@@ -164,7 +172,10 @@
             this.toolStripSeparator1,
             this.distanceMeasurementToolStripMenuItem,
             this.areaMeasurementToolStripMenuItem,
-            this.clearMeasurementToolStripMenuItem});
+            this.clearMeasurementToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.signalCharacterToolStripMenuItem,
+            this.signalCharacterResetToolStripMenuItem});
             this.устройствоToolStripMenuItem.Name = "устройствоToolStripMenuItem";
             this.устройствоToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
             this.устройствоToolStripMenuItem.Text = "Устройство";
@@ -173,20 +184,20 @@
             // 
             this.tESLABS300ToolStripMenuItem.CheckOnClick = true;
             this.tESLABS300ToolStripMenuItem.Name = "tESLABS300ToolStripMenuItem";
-            this.tESLABS300ToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.tESLABS300ToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
             this.tESLABS300ToolStripMenuItem.Text = "TESLA BS300";
             this.tESLABS300ToolStripMenuItem.CheckedChanged += new System.EventHandler(this.tESLABS300ToolStripMenuItem_CheckedChanged);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(199, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(260, 6);
             // 
             // distanceMeasurementToolStripMenuItem
             // 
             this.distanceMeasurementToolStripMenuItem.CheckOnClick = true;
             this.distanceMeasurementToolStripMenuItem.Name = "distanceMeasurementToolStripMenuItem";
-            this.distanceMeasurementToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.distanceMeasurementToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
             this.distanceMeasurementToolStripMenuItem.Text = "Измерение расстояния";
             this.distanceMeasurementToolStripMenuItem.CheckedChanged += new System.EventHandler(this.distanceMeasurementToolStripMenuItem_CheckedChanged);
             // 
@@ -194,16 +205,35 @@
             // 
             this.areaMeasurementToolStripMenuItem.CheckOnClick = true;
             this.areaMeasurementToolStripMenuItem.Name = "areaMeasurementToolStripMenuItem";
-            this.areaMeasurementToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.areaMeasurementToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
             this.areaMeasurementToolStripMenuItem.Text = "Измерение площади";
             this.areaMeasurementToolStripMenuItem.CheckedChanged += new System.EventHandler(this.areaMeasurementToolStripMenuItem_CheckedChanged);
             // 
             // clearMeasurementToolStripMenuItem
             // 
             this.clearMeasurementToolStripMenuItem.Name = "clearMeasurementToolStripMenuItem";
-            this.clearMeasurementToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.clearMeasurementToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
             this.clearMeasurementToolStripMenuItem.Text = "Очистить измерения";
             this.clearMeasurementToolStripMenuItem.Click += new System.EventHandler(this.clearMeasurementToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(260, 6);
+            // 
+            // signalCharacterToolStripMenuItem
+            // 
+            this.signalCharacterToolStripMenuItem.Name = "signalCharacterToolStripMenuItem";
+            this.signalCharacterToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.signalCharacterToolStripMenuItem.Text = "Характеристики сигнала";
+            this.signalCharacterToolStripMenuItem.Click += new System.EventHandler(this.signalCharacterToolStripMenuItem_Click);
+            // 
+            // signalCharacterResetToolStripMenuItem
+            // 
+            this.signalCharacterResetToolStripMenuItem.Name = "signalCharacterResetToolStripMenuItem";
+            this.signalCharacterResetToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.signalCharacterResetToolStripMenuItem.Text = "Сбросить характеристики сигнала";
+            this.signalCharacterResetToolStripMenuItem.Click += new System.EventHandler(this.signalCharacterResetToolStripMenuItem_Click);
             // 
             // справкаToolStripMenuItem
             // 
@@ -375,11 +405,62 @@
             // 
             this.saveFileDialog.FileName = "tesla-bs300.bmp";
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.DecimalPlaces = 4;
+            this.numericUpDown1.Location = new System.Drawing.Point(673, 418);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(95, 20);
+            this.numericUpDown1.TabIndex = 18;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.DecimalPlaces = 4;
+            this.numericUpDown2.Location = new System.Drawing.Point(550, 418);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown2.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(438, 341);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(172, 71);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.verticalLine);
@@ -408,6 +489,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.teslaStatePictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.horizontalLine)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.verticalLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -449,6 +532,12 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem signalCharacterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem signalCharacterResetToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
